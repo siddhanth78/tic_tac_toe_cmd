6f77bdaacf30d72e29a0b269b05cb49196c2e5be
Xstinkshun <61350672+siddhanth78@users.noreply.github.com>
2020-11-20 12:54:50 +0530

    Add files via upload

---
+++ tic_tac_toe_cmd.py
@@ -0,0 +1,123 @@
+import numpy as np
+import os
+
+board = np.array([["-","-","-"],
+					["-","-","-"],
+					["-","-","-"]])
+
+win = [[1,2,3],
+		[4,5,6],
+		[7,8,9],
+		[1,4,7],
+		[2,5,8],
+		[3,6,9],
+		[1,5,9],
+		[3,5,7]]
+		
+p1moves = []
+p2moves = []
+
+lisp1 = []
+lisp2 = []
+
+ct=0
+
+def checkwin(pmoves,lisp,win):
+	winner=0
+	li = []
+	for x in pmoves:
+		li.append(x)
+		for n in pmoves:
+			if n in li:
+				li = [x]
+				continue
+			else:
+				li.append(n)
+				for j in pmoves:
+					if j in li:
+						li = [x,n]
+						continue
+					else:
+						li.append(j)
+					li.sort()
+					if li in lisp:
+						pass
+					else:
+						if len(li)<3:
+							pass
+						else:
+							lisp.append(li)
+					li = [x,n]
+			li = [x]
+		li = []
+	
+	for m in lisp:
+		if m in win:
+			winner=1
+			break
+		else:
+			winner=0
+			
+	return lisp,pmoves,winner,win
+	
+turn=1
+winner=0
+
+print("\nTic-Tac-Toe\nTo play your move, enter a number from 1-9.")
+print()
+print(board)
+print()
+
+while ct<9:
+	try:
+		player = int(input("P{}: ".format(turn)))
+	except:
+		print("Invalid choice.")
+		continue
+	else:
+		pass
+		
+	if 0<player<4:
+		row = 0
+		cell = player-1
+	elif 4<=player<7:
+		row = 1
+		cell = player-4
+	elif 7<=player<10:
+		row = 2
+		cell = player-7
+		
+	if board[row][cell] != "-":
+		print("Invalid choice.")
+		continue
+		
+	ct+=1
+		
+	if turn == 1:
+		p1moves.append(player)
+		board[row][cell] = "X"
+		print()
+		print(board)
+		print()
+		lisp1,p1moves,winner,win = checkwin(p1moves,lisp1,win)
+		if winner==1:
+			print("P1 wins!\n")
+			quit()
+		else:
+			turn=2
+			continue
+	elif turn == 2:
+		p2moves.append(player)
+		board[row][cell] = "O"
+		print()
+		print(board)
+		print()
+		lisp2,p2moves,winner,win = checkwin(p2moves,lisp2,win)
+		if winner==1:
+			print("P2 wins!\n")
+			quit()
+		else:
+			turn=1
+			continue
+		
+print("Draw!\n")
